8985493f7b41096ee9311aaac365b56e32985397
abhijeetTehra <abhijeet.t@gaiansolutions.com>
2024-04-19 13:59:00 +0530

    Added welcome msg

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 4200;
 app.use(express.json());
 
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/index.html");
+	// res.sendFile(__dirname + "/index.html");
+	res.send({
+		message: "Welcome to ZoomBot Puppeteer Service"
+	})
 });
 
 app.use("/api", streamRouter);
