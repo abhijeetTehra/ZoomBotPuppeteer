a12137b56efb54f463a837e601ca9aa26816e287
syedgaian <syed.s@gaiansolutions.com>
2024-04-23 19:33:22 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -32,23 +32,23 @@ startPuppeteerFunction(pages, wss);
 
 server.on("upgrade", function upgrade(request, socket, head) {
 	upgrade++;
-    pathname = new URL(
-      request.url,
-      `http://${request.headers.host}`
-    ).pathname.split("/")[1];
-    if(upgrade!=1){
-      return;
-    }
-    console.log("Upgrade requested for", pathname);
+	pathname = new URL(
+		request.url,
+		`http://${request.headers.host}`
+	).pathname.split("/")[1];
+	if (upgrade != 1) {
+		return;
+	}
+	console.log("Upgrade requested for", pathname);
 
-    if (pathname===uniqueId && upgrade==1) {
-      // Check if the path has been initialized
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit("connection", ws, request, pathname);
-      });
-    } else {
-      socket.destroy(); // Destroy the socket if the path is not recognized
-    }
+	if (pathname === uniqueId && upgrade == 1) {
+		// Check if the path has been initialized
+		wss.handleUpgrade(request, socket, head, (ws) => {
+			wss.emit("connection", ws, request, pathname);
+		});
+	} else {
+		socket.destroy(); // Destroy the socket if the path is not recognized
+	}
 });
 console.log("Rendered");
 server.listen(port, () => {
--- src/utils/startPuppeteer.js
+++ src/utils/startPuppeteer.js
@@ -5,74 +5,73 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const startPuppeteerFunction = async (pages, wss) => {
+	const url = process.env.URL;
+	const uniqueId = process.env.UNIQUEID;
+	const path = uniqueId;
 
-  const url = process.env.URL;
-  const uniqueId = process.env.UNIQUEID;
-  const path = uniqueId;
+	if (pages[path]) {
+		return res.send({
+			message: "WebSocket channel already exists for this name",
+		});
+	}
 
-  if (pages[path]) {
-    return res.send({
-      message: "WebSocket channel already exists for this name",
-    });
-  }
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: [
+			"--disable-notifications",
+			"--enable-automation",
+			"--start-maximized",
+		],
+		defaultViewport: { width: 1280, height: 720 },
+	});
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--disable-notifications",
-      "--enable-automation",
-      "--start-maximized",
-    ],
-    defaultViewport: { width: 1280, height: 720 },
-  });
+	const page = await browser.newPage();
+	await page.setUserAgent(
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+	);
+	await page.goto(url);
 
-  const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
-  );
-  await page.goto(url);
+	console.log("Browser started for", uniqueId);
 
-  console.log("Browser started for", uniqueId);
+	const time = parseInt(1000 / 30); // ~33.33 milliseconds
 
-  const time = parseInt(1000 / 30); // ~33.33 milliseconds
+	// Store the page and settings under the path
+	pages[path] = { page, time, browser };
 
-  // Store the page and settings under the path
-  pages[path] = { page, time, browser };
+	wss.on("connection", function connection(ws, request) {
+		console.log("WebSocket connection established on", path);
 
-  wss.on("connection", function connection(ws, request) {
-    console.log("WebSocket connection established on", path);
+		ws.on("message", async (message) => {
+			console.log(`Received message from ${pathname}:`, message);
+			try {
+				await pages[pathname].page.focus("body");
+				await pages[pathname].page.keyboard.down("ControlLeft");
+				await pages[pathname].page.keyboard.press("KeyM");
+				await pages[pathname].page.keyboard.up("ControlLeft");
+			} catch (e) {
+				console.log("PAGE NOT FOUND");
+				console.log(pages);
+			}
+		});
 
-    ws.on("message", async (message) => {
-      console.log(`Received message from ${pathname}:`, message);
-      try {
-        await pages[pathname].page.focus("body");
-        await pages[pathname].page.keyboard.down("ControlLeft");
-        await pages[pathname].page.keyboard.press("KeyM");
-        await pages[pathname].page.keyboard.up("ControlLeft");
-      } catch (e) {
-        console.log("PAGE NOT FOUND");
-        console.log(pages);
-      }
-    });
+		ws.on("close", () => {
+			console.log(`WebSocket connection closed on ${pathname}`);
+		});
 
-    ws.on("close", () => {
-      console.log(`WebSocket connection closed on ${pathname}`);
-    });
-
-    // Set an interval to send screenshots
-    const interval = setInterval(async () => {
-      if (pages[pathname]) {
-        const screenshotBuffer = await pages[pathname].page.screenshot({
-          encoding: "binary",
-        });
-        if (ws.readyState === WebSocket.OPEN) {
-          ws.send(screenshotBuffer);
-        }
-      } else {
-        clearInterval(interval); // Clear the interval if the page is not found
-      }
-    }, [1000/30]);
-  });
+		// Set an interval to send screenshots
+		const interval = setInterval(async () => {
+			if (pages[pathname]) {
+				const screenshotBuffer = await pages[pathname].page.screenshot({
+					encoding: "binary",
+				});
+				if (ws.readyState === WebSocket.OPEN) {
+					ws.send(screenshotBuffer);
+				}
+			} else {
+				clearInterval(interval); // Clear the interval if the page is not found
+			}
+		}, 1000 / 60);
+	});
 };
 
 module.exports = { startPuppeteerFunction };
